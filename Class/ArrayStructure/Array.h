a63257e4a42c4e4d53a4218d27295208064d8329
sbeltran94 <steven_beltran@rocketmail.com>
2015-03-25 13:48:53 -0700

    Signed-off-by: sbeltran94 <steven_beltran@rocketmail.com>

---
+++ Class/ArrayStructure/Array.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   Array.h
+ * Author: rcc
+ *
+ * Created on March 25, 2015, 12:58 PM
+ */
+
+#ifndef ARRAY_H
+#define	ARRAY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ARRAY_H */
+
